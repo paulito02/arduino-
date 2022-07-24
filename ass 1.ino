110bb93ca232281ad5ea0c458f6537517227e435
paulito02 <71513408+paulito02@users.noreply.github.com>
2022-07-24 19:58:09 +0100

    Add files via upload
    creating binary number

---
+++ ass 1.ino
@@ -0,0 +1,108 @@
+void setup() {
+ pinMode(1,OUTPUT) 
+ pinMode(2,OUTPUT) 
+ pinMode(3,OUTPUT) 
+ pinMode(4,OUTPUT)         // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(1, LOW)
+digitalWrite(2, LOW)
+digitalWrite(3, LOW)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, LOW)
+digitalWrite(2, LOW)
+digitalWrite(3, LOW)
+digitalWrite(4, HIGH)
+dalay(1000)
+
+
+digitalWrite(1, LOW)
+digitalWrite(2, LOW)
+digitalWrite(3, HIGH)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, LOW)
+digitalWrite(2, LOW)
+digitalWrite(3, HIGH)
+digitalWrite(4, HIGH)
+dalay(1000)
+
+digitalWrite(1, LOW)
+digitalWrite(2, HIGH)
+digitalWrite(3, LOW)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, LOW)
+digitalWrite(2, HIGH)
+digitalWrite(3, LOW)
+digitalWrite(4, HIGH)
+dalay(1000)
+
+digitalWrite(1, LOW)
+digitalWrite(2, HIGH)
+digitalWrite(3, HIGH)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, LOW)
+digitalWrite(2, HIGH)
+digitalWrite(3, HIGH)
+digitalWrite(4, HIGH)
+dalay(1000)
+
+digitalWrite(1, HIGH)
+digitalWrite(2, LOW)
+digitalWrite(3, LOW)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, HIGH)
+digitalWrite(2, LOW)
+digitalWrite(3, LOW)
+digitalWrite(4, HOGH)
+dalay(1000)
+
+digitalWrite(1, HIGH)
+digitalWrite(2, LOW)
+digitalWrite(3, HIGH)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, HIGH)
+digitalWrite(2, LOW)
+digitalWrite(3, HIGH)
+digitalWrite(4, HIGH)
+dalay(1000)
+
+digitalWrite(1, HIGH)
+digitalWrite(2, HIGH)
+digitalWrite(3, LOW)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, HIGH)
+digitalWrite(2, HIGH)
+digitalWrite(3, HIGH)
+digitalWrite(4, LOW)
+dalay(1000)
+
+digitalWrite(1, HIGH)
+digitalWrite(2, HIGH)
+digitalWrite(3, HIGH)
+digitalWrite(4, HIGH)
+dalay(1000)
+
+
+
+
+
+
+
+}
