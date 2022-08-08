11a34b22b77da15c82815349d3cc73181c7c6dca
paulito02 <71513408+paulito02@users.noreply.github.com>
2022-08-08 21:20:11 +0100

    Add files via upload
    making use of for loop on a board

---
+++ for loop.ino
@@ -0,0 +1,43 @@
+// int yellowled=6;
+// int redled=9;
+// int yellowTime=500;
+// int redTime=500;
+// int yelloBlink=3;
+// int redBlink=5
+
+// void setup() {
+  
+//   // put your setup code here, to run once:
+// pinMode(yellowpin, OUTPUT);
+// pinMode(redled, OUTPUT);
+// serial.begin(9600);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   for(j=1;j<=yellowBlink;j=j+1){
+// digitalWrite(yellowled, HIGH);
+// delay(yelloTime);
+// digitalWrite(yellowled, LOW);
+//    }
+//   for(j=1;j<=redBlink;j=j+1){
+// digitalWrite(redled, HIGH);
+// delay(redTime);
+// digitalWrite(redLed, LOW);
+// delay(redTime);
+// }
+
+
+int j;
+int delayTime=500;
+void setup(){  
+  serial.begin(9600);
+}
+
+void loop( {
+for (j=1;j<=10;j=j+1){
+  serial.printin(j);
+  delay(delayTime);
+}
+serial.printin();
+}
