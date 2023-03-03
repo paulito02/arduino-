5fcd5051c4f286933e4ab78c29683f184c8c033d
paulito02 <71513408+paulito02@users.noreply.github.com>
2023-03-03 20:33:07 +0100

    Update getting_tone_from_an_active_buzzer.ino
    added a comment to the for loop

--- getting_tone_from_an_active_buzzer.ino
+++ getting_tone_from_an_active_buzzer.ino
@@ -9,6 +9,7 @@ pinMode (buzzPin, OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // the for loop set j to be 1 and also initiate a count from 1 to 100 by adding the initial state by 1 till it gets to 100 the the loop is fa;se and it will break out
 for (j=1; j<=100; j=j+1){
   digitalWrite(buzzPin, HIGH);
   delay(dt1);
